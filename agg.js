87890407dcac9248f21fa139f98ed9e6ebecd68f
Jackson <sp@liberstudio.com>
2017-07-21 20:15:19 +0200

    Update agg.js

--- agg.js
+++ agg.js
@@ -2,5 +2,5 @@ var aggiornaScript = {
 mioPulsante: '1.3',
 mioPulsanteNew: 'Change z-index from 99 to 15!!',
 bigInventor: '1.1',
-bigInventorNew: '<ul><li><b>IT:</b> <i>Le icone nel inventario cambiate.</i></li><li><b>EN:</b> <i>Inventory icons changed</i></li><li><b>RU:</b> <i>Замена оконок в инвентаре.</i></li><li><b>RO:</b><i>Pictogramele schimbate in inventar.</i></li></ul>',
+bigInventorNew: '<ul><li><b>IT:</b> <i>Le icone nel inventario cambiate.</i></li><li><b>EN:</b> <i>Inventory icons changed.</i></li><li><b>RU:</b> <i>Замена оконок в инвентаре.</i></li><li><b>RO:</b><i>Pictogramele schimbate in inventar.</i></li></ul>',
 };
