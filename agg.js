a05d55d7419675a7580aac6b87571b178f8a9b93
Jackson <sp@liberstudio.com>
2017-09-08 02:00:26 +0300

    Update agg.js

--- agg.js
+++ agg.js
@@ -1,6 +1,6 @@
 var aggiornaScript = {
 mioPulsante: '1.3',
 mioPulsanteNew: 'Change z-index from 99 to 15!!',
-bigInventor: '1.2',
-bigInventorNew: '<ul><li><b>IT:</b> <i>Aggiunti 4 icone nuove(Ispirato da TW Collections).</i></li><li><b>EN:</b> <i>Added 4 filter icons (inspired by TW Collections! Thx to Dun).</i></li><li><b>RU:</b> <i>Добавлены 4 иконки(Inspired by TW Collections!THx to Dun).</i></li><li><b>RO:</b><i>Adăugat 4 pictograme.</i></li></ul>',
+bigInventor: '1.3',
+bigInventorNew: '<ul><li><b>IT:</b> <i>Agginto lingua tedesca.</i></li><li><b>EN:</b> <i>Added German Language  and remove italian default description of the category.</i></li><li><b>RU:</b> <i>Добавлен немецкий язык, удалено италянское наименование шмотов.</i></li><li><b>RO:</b><i>Adăugat limba nemtilor :D si am scos descrierea inventariului in limba italiana.</i></li></ul>',
 };
