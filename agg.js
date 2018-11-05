8b89c3e93b681d960c03b9cf6581083930465e1e
Jackson <sp@liberstudio.com>
2018-11-05 10:43:33 +0100

    Update agg.js

--- agg.js
+++ agg.js
@@ -1,6 +1,6 @@
 var aggiornaScript = {
 mioPulsante: '1.3',
 mioPulsanteNew: 'Change z-index from 99 to 15!!',
-bigInventor: '1.3',
-bigInventorNew: '<ul><li><b>IT:</b> <i>Agginto lingua tedesca.</i></li><li><b>EN:</b> <i>Added German Language  and remove italian default description of the category.</i></li><li><b>RU:</b> <i>Добавлен немецкий язык, удалено италянское наименование шмотов.</i></li><li><b>RO:</b><i>Adăugat limba nemtilor :D si am scos descrierea inventariului in limba italiana.</i></li></ul>',
+bigInventor: '1.4',
+bigInventorNew: '<ul><li><b>IT:</b> <i>Nuovo hosting del cache.</i></li><li><b>EN:</b> <i>New host for cached images</i></li><li><b>RU:</b> <i>Добавлен хост для картинок</i></li><li><b>RO:</b><i>Adăugat host nou pentru imagini.</i></li></ul>',
 };
