90a344b3168b6b6d4c2592c5f680158821b83640
Jackson <sp@liberstudio.com>
2018-11-05 10:57:50 +0100

    Update agg.js

--- agg.js
+++ agg.js
@@ -1,6 +1,6 @@
 var aggiornaScript = {
 mioPulsante: '1.3',
 mioPulsanteNew: 'Change z-index from 99 to 15!!',
-bigInventor: '1.4',
+bigInventor: '1.5',
 bigInventorNew: '<ul><li><b>IT:</b> <i>Nuovo hosting del cache.</i></li><li><b>EN:</b> <i>New host for cached images</i></li><li><b>RU:</b> <i>Добавлен хост для картинок</i></li><li><b>RO:</b><i>Adăugat host nou pentru imagini.</i></li></ul>',
 };
