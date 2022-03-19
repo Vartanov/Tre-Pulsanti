fb9da7b6d9a135bfa2009142a0340dcd734eb2da
Liber Design <liberdesignfano@gmail.com>
2022-03-19 13:36:16 +0100

    Agg. Versione!

--- agg.js
+++ agg.js
@@ -1,6 +1,6 @@
 var aggiornaScript = {
-mioPulsante: '1.7',
-mioPulsanteNew: 'Change hosting!',
-bigInventor: '1.7',
-bigInventorNew: '<ul><li><b>IT:</b> <i>Nuovo hosting del cache.</i></li><li><b>EN:</b> <i>New host for cached images</i></li><li><b>RU:</b> <i>Добавлен хост для картинок</i></li><li><b>RO:</b><i>Adăugat host nou pentru imagini.</i></li></ul>',
+    mioPulsante: '1.7',
+    mioPulsanteNew: 'Change hosting!',
+    bigInventor: '1.7',
+    bigInventorNew: '<ul><li><b>IT:</b> <i>Aggiornamento della versione</i></li><li><b>EN:</b> <i>Version update</i></li><li><b>RU:</b> <i>Обновление версии</i></li><li><b>RO:</b><i>Actualizare versiune.</i></li></ul>',
 };
