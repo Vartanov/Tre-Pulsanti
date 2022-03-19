8545a019338f8a42dac2c143db38d23edd34e520
Liber Design <liberdesignfano@gmail.com>
2022-03-19 13:34:48 +0100

    Fix Version!

--- agg.js
+++ agg.js
@@ -1,6 +1,6 @@
 var aggiornaScript = {
-mioPulsante: '1.3',
+mioPulsante: '1.7',
 mioPulsanteNew: 'Change hosting!',
-bigInventor: '1.6',
+bigInventor: '1.7',
 bigInventorNew: '<ul><li><b>IT:</b> <i>Nuovo hosting del cache.</i></li><li><b>EN:</b> <i>New host for cached images</i></li><li><b>RU:</b> <i>Добавлен хост для картинок</i></li><li><b>RO:</b><i>Adăugat host nou pentru imagini.</i></li></ul>',
 };
