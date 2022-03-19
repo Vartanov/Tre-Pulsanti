dec82d7d817943c3e712ab2676caf2cee8ba2c60
Liber Design <liberdesignfano@gmail.com>
2022-03-19 13:38:10 +0100

    Version update

--- agg.js
+++ agg.js
@@ -1,6 +1,6 @@
 var aggiornaScript = {
     mioPulsante: '1.7',
-    mioPulsanteNew: 'Change hosting!',
+    mioPulsanteNew: 'Version update!',
     bigInventor: '1.7',
     bigInventorNew: '<ul><li><b>IT:</b> <i>Aggiornamento della versione</i></li><li><b>EN:</b> <i>Version update</i></li><li><b>RU:</b> <i>Обновление версии</i></li><li><b>RO:</b><i>Actualizare versiune.</i></li></ul>',
 };
